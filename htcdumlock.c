f0e9e758832518c7e5f57e4c4452beaf1af271e4
Dees Troy <dees_troy@teamw.in>
2012-02-22 14:55:02 -0500

    Updates to make dumlock work with any recovery

--- htcdumlock.c
+++ htcdumlock.c
@@ -33,106 +33,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-#define CMDLINE_SERIALNO        "androidboot.serialno="
-#define CMDLINE_SERIALNO_LEN    (strlen(CMDLINE_SERIALNO))
-#define CPUINFO_SERIALNO        "Serial"
-#define CPUINFO_SERIALNO_LEN    (strlen(CPUINFO_SERIALNO))
-#define CPUINFO_HARDWARE        "Hardware"
-#define CPUINFO_HARDWARE_LEN    (strlen(CPUINFO_HARDWARE))
-
-char device_id[255];
 int verbose = 0, java = 0;
-
-void get_device_id(void)
-{
-	FILE *fp;
-    char line[2048];
-	char hardware_id[32];
-	char* token;
-
-    // Assign a blank device_id to start with
-    device_id[0] = 0;
-
-    // First, try the cmdline to see if the serial number was supplied
-	fp = fopen("/proc/cmdline", "rt");
-	if (fp != NULL)
-    {
-        // First step, read the line. For cmdline, it's one long line
-        fgets(line, sizeof(line), fp);
-        fclose(fp);
-
-        // Now, let's tokenize the string
-        token = strtok(line, " ");
-
-        // Let's walk through the line, looking for the CMDLINE_SERIALNO token
-        while (token)
-        {
-            // We don't need to verify the length of token, because if it's too short, it will mismatch CMDLINE_SERIALNO at the NULL
-            if (memcmp(token, CMDLINE_SERIALNO, CMDLINE_SERIALNO_LEN) == 0)
-            {
-                // We found the serial number!
-                strcpy(device_id, token + CMDLINE_SERIALNO_LEN);
-                return;
-            }
-            token = strtok(NULL, " ");
-        }
-    }
-
-	// Now we'll try cpuinfo for a serial number
-	fp = fopen("/proc/cpuinfo", "rt");
-	if (fp != NULL)
-    {
-		while (fgets(line, sizeof(line), fp) != NULL) { // First step, read the line.
-			if (memcmp(line, CPUINFO_SERIALNO, CPUINFO_SERIALNO_LEN) == 0)  // check the beginning of the line for "Serial"
-			{
-				// We found the serial number!
-				token = line + CPUINFO_SERIALNO_LEN; // skip past "Serial"
-				while (((int)*token > 0 && (int)*token <= 32 ) || (int)*token == ':') token++; // skip over all spaces and the colon
-				if (*token != NULL) {
-                    token[30] = 0;
-					if (token[strlen(token)-1] == 10) { // checking for endline chars and dropping them from the end of the string if needed
-						memset(device_id, 0, sizeof(device_id));
-						strncpy(device_id, token, strlen(token) - 1);
-					} else {
-						strcpy(device_id, token);
-					}
-					if (verbose)
-						printf("serial from cpuinfo: '%s'\n", device_id);
-					fclose(fp);
-					return;
-				}
-			} else if (memcmp(line, CPUINFO_HARDWARE, CPUINFO_HARDWARE_LEN) == 0) {// We're also going to look for the hardware line in cpuinfo and save it for later in case we don't find the device ID
-				// We found the hardware ID
-				token = line + CPUINFO_HARDWARE_LEN; // skip past "Hardware"
-				while (((int)*token > 0 && (int)*token <= 32 ) || (int)*token == ':')  token++; // skip over all spaces and the colon
-				if (*token != NULL) {
-                    token[30] = 0;
-					if (token[strlen(token)-1] == 10) { // checking for endline chars and dropping them from the end of the string if needed
-                        memset(hardware_id, 0, sizeof(hardware_id));
-						strncpy(hardware_id, token, strlen(token) - 1);
-					} else {
-						strcpy(hardware_id, token);
-					}
-					if (verbose)
-						printf("hardware id from cpuinfo: '%s'\n", hardware_id);
-				}
-			}
-		}
-		fclose(fp);
-    }
-	
-	if (hardware_id[0] != 0) {
-		if (verbose)
-			printf("using hardware id for device id: '%s'\n", hardware_id);
-		strcpy(device_id, hardware_id);
-		return;
-	}
-
-    strcpy(device_id, "serialno");
-	if (verbose)
-		printf("device id not found, using '%s'.", device_id);
-    return;
-}
 
 void reboot_device() {
 	// Reboot
@@ -141,7 +42,7 @@ void reboot_device() {
 }
 
 void flash_recovery_to_boot(int no_flash, int no_reboot) {
-	char twrp_device_path[255], recovery_path[255], boot_path[255],
+	char device_path[255], recovery_path[255], boot_path[255],
 		exec[255], md5recovery[255], md5boot[255],
 		recoveryimg[255], bootimg[255], tempimg[255];
 	int ret_val = 0;
@@ -150,38 +51,29 @@ void flash_recovery_to_boot(int no_flash, int no_reboot) {
 
 	// Create folders
 	if (verbose)
-		printf("Making '/sdcard/TWRP'\n");
-	mkdir("/sdcard/TWRP", 0777);
-	if (verbose)
-		printf("Making folder '/sdcard/TWRP/htcdumlock'\n");
-	mkdir("/sdcard/TWRP/htcdumlock", 0777);
-	strcpy(twrp_device_path, "/sdcard/TWRP/htcdumlock/");
-	strcat(twrp_device_path, device_id);
-	if (verbose)
-		printf("Making folder '%s'\n", twrp_device_path);
-	mkdir(twrp_device_path, 0777);
+		printf("Making folder '/sdcard/htcdumlock'\n");
+	mkdir("/sdcard/htcdumlock", 0777);
+	strcpy(device_path, "/sdcard/htcdumlock/");
 	// Make folder for recovery
-	strcpy(recovery_path, twrp_device_path);
-	strcat(recovery_path, "/recovery");
+	strcpy(recovery_path, device_path);
+	strcat(recovery_path, "recovery/");
 	if (verbose)
 		printf("Making folder '%s'\n", recovery_path);
 	mkdir(recovery_path, 0777);
-	strcat(recovery_path, "/");
 	// Make folder for boot
-	strcpy(boot_path, twrp_device_path);
-	strcat(boot_path, "/boot");
+	strcpy(boot_path, device_path);
+	strcat(boot_path, "boot/");
 	if (verbose)
 		printf("Making folder '%s'\n", boot_path);
 	mkdir(boot_path, 0777);
-	strcat(boot_path, "/");
 
 	// Set up file locations
 	strcpy(recoveryimg, recovery_path);
 	strcat(recoveryimg, "recovery.img");
 	strcpy(bootimg, boot_path);
 	strcat(bootimg, "boot.img");
-	strcpy(tempimg, twrp_device_path);
-	strcat(tempimg, "/temp.img");
+	strcpy(tempimg, device_path);
+	strcat(tempimg, "temp.img");
 
 	// Dump recovery
 	strcpy(exec, "dump_image recovery ");
@@ -256,6 +148,19 @@ void flash_recovery_to_boot(int no_flash, int no_reboot) {
 		return;
 	}
 
+	// Move boot.img into /data
+	strcpy(exec, "cp ");
+	strcat(exec, bootimg);
+	strcat(exec, " ");
+	strcat(exec, "/data/boot.img");
+	if (verbose)
+		printf("Copying boot.img to /data: '%s'\n", exec);
+	ret_val = system(exec);
+	if (ret_val != 0) {
+		printf("Unable to copy boot to /data: '%s'\nFailed\n");
+		return;
+	}
+
 	// Flash recovery to boot
 	strcpy(exec, "flash_image boot ");
 	strcat(exec, recoveryimg);
@@ -278,21 +183,20 @@ void flash_recovery_to_boot(int no_flash, int no_reboot) {
 
 void restore_original_boot(int no_flash) {
 	char boot_path[255], exec[255];
+	int ret_val;
 
 	// Restore original boot partition
-	strcpy(boot_path, "/sdcard/TWRP/htcdumlock/");
-	strcat(boot_path, device_id);
-	strcat(boot_path, "/boot/");
-	strcpy(exec, "flash_image boot ");
-	strcat(exec, boot_path);
-	strcat(exec, "boot.img");
+	strcpy(exec, "flash_image boot /sdcard/htcdumlock/boot/boot.img");
 	if (no_flash) {
 		if (verbose)
 			printf("NOT restoring boot due to argument 'noflash', command is '%s'\n", exec);
 	} else {
 		if (verbose)
 			printf("Running command: '%s'\n", exec);
-		system(exec);
+		ret_val = system(exec);
+		if (ret_val != 0) {
+			printf("Unable to restore boot.\nFailed\n");
+		}
 	}
 }
 
@@ -335,15 +239,6 @@ int main(int argc, char** argv)
 		printf("  noreboot -- does not reboot after flashing boot during 'recovery'\n");
 		printf("  verbose  -- show extra debug information\n");
 		printf("\nNOTE: You cannot do 'recovery' and 'restore' in the same operation.\nFailed\n");
-		return 0;
-	}
-
-	get_device_id();
-	if (verbose)
-		printf("Device ID is: '%s'\n", device_id);
-	if (strcmp(device_id, "0000000000000000") == 0) {
-		printf("Error, device ID is all zeros!\n");
-		printf("Did you 'su' first? HTC Dumlock requires root access.\nFailed\n");
 		return 0;
 	}
 
